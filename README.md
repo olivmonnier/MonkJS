203d6321c6ed196ab0964562232c90810577e176
Oliv75 <omonnier75@gmail.com>
2014-09-12 00:47:22 +0200

    [UPDATE] several methods re create & add some

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ MonkJS
 bower install monkjs
 ```
 ## How it works
+### Class
+#### How to create a class
 ```javascript
 var Class = $Class(null, {
   __construct : function(){
@@ -12,5 +14,52 @@ var Class = $Class(null, {
   }
 });
 
-var obj = new Class();
+var instance = new Class();
+```
+#### Methods of Class
+Constructor method
+```javascript
+```
+Instance method
+```javascript
+```
+ExtendClass method
+```javascript
+```
+Include method
+```javascript
+```
+#### Methods of Instance
+Extend method
+```javascript
+```
+Watch method
+```javascript
+```
+Unwatch method
+```javascript
+```
+GetAttributes method
+```javascript
+```
+### Decorator class
+#### How use it
+```javascript
+```
+### Policies functions
+#### After method
+```javascript
+```
+#### Before method
+```javascript
+```
+### List of functions
+extend method
+```javascript
+```
+include method
+```javascript
+```
+flip method
+```javascript
 ```
--- dist/monkjs.js
+++ dist/monkjs.js
@@ -1,11 +1,48 @@
 (function() {
-  var Class, root;
+  var Class, Decorator, Policies, fn, root;
+
+  fn = {
+    flip: function(fn){
+      return function(first, second){
+        if(arguments.length === 2){
+          return fn.call(second, first);
+        }
+        else{
+          return function(second){
+            return fn.call(second, first);
+          };
+        }
+      };
+    },
+    extend: function(consumer, provider){
+      var key;
+      for(key in provider){
+        if(provider.hasOwnProperty(key)){
+          consumer[key] = provider[key];
+        }
+      }
+      return consumer;
+    },
+    include: function(consumer, provider){
+      var key;
+      for(key in provider){
+        if(provider.hasOwnProperty(key)){
+          consumer.prototype[key] = provider[key];
+        }
+      }
+      return consumer;
+    }
+  };
 
   Class = function(Parent, props) {
     var Child, F, i;
     Child = void 0;
     F = void 0;
     i = void 0;
+    if (props.hasOwnProperty("instance")){
+      var instance = props.instance;
+      delete props.instance;
+    }
     Child = function() {
       if (Child.surrogate && Child.surrogate.hasOwnProperty("__construct")) {
         Child.surrogate.__construct.apply(this, arguments);
@@ -13,18 +50,17 @@
       if (Child.prototype.hasOwnProperty("__construct")) {
         Child.prototype.__construct.apply(this, arguments);
       }
-    };
-    Child.extendClass = function(obj) {
-      var n;
-      for (n in obj) {
-        Child[n] = obj[n];
+      if(instance !== undefined){
+        if(typeof instance === "object"){
+          fn.extend(this, instance);
+        }
       }
+    };
+    Child.extendClass = function(obj){
+      return fn.extend(Child, obj);
     };
     Child.include = function(obj) {
-      var n;
-      for (n in obj) {
-        Child.prototype[n] = obj[n];
-      }
+      return fn.include(Child, obj);
     };
     Parent = Parent || Object;
     F = function() {};
@@ -33,10 +69,7 @@
     Child.surrogate = Parent.prototype;
     Child.prototype.$constructor = Child;
     Child.prototype.$extend = function(obj) {
-      var n;
-      for (n in obj) {
-        this[n] = obj[n];
-      }
+      return fn.extend(this, obj);
     };
     Child.prototype.$watch = function(prop, handler) {
       var getter, newval, oldval, setter;
@@ -57,23 +90,70 @@
           configurable: true
         });
       }
+      return this;
     };
     Child.prototype.$unwatch = function(prop) {
       var val;
       val = this[prop];
       delete this[prop];
       this[prop] = val;
+      return this;
     };
-    for (i in props) {
-      if (props.hasOwnProperty(i)) {
-        Child.prototype[i] = props[i];
+    Child.prototype.$getAttributes = function(){
+      var key, array = [];
+      for(key in this){
+        if(typeof this[key] !== 'function'){
+          var obj = {};
+          obj[key] = this[key];
+          array.push(obj);
+        }
+      }
+      return array;
+    };
+    return fn.include(Child, props);
+  };
+
+  Decorator = fn.flip(function(decoration){
+    function Decorated(){
+      var self = this instanceof Decorated ? this : new Decorated();
+      return clazz.apply(self, arguments);
+    }
+    var key, instance = new clazz(),
+        deco = new Decorated();
+    for(key in instance){
+      if (deco.hasOwnProperty(key)){
+        delete instance[key];
       }
     }
-    return Child;
+    Decorated.prototype = fn.extend(instance, decoration);
+    return Decorated;
+  });
+
+  Policies = {
+    after: function(decoration){
+      return function(method){
+        return function(){
+          var value = method.apply(this, arguments);
+          decoration.call(this, value);
+          return value;
+        };
+      };
+    },
+    before: function(decoration){
+      return function(method){
+        return function(){
+          decoration.apply(this, arguments);
+          return method.apply(this, arguments);
+        };
+      };
+    }
   };
 
   root = typeof exports !== "undefined" && exports !== null ? exports : window;
 
   root.$Class = Class;
+  root.$Decorator = Decorator;
+  root.$Policies = Policies;
+  root.$fn = fn;
 
 }).call(this);
