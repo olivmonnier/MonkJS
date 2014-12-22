1aba3ad78d835dd5f33278ebbe024644e9bd0178
Oliv75 <omonnier75@gmail.com>
2014-12-22 09:46:50 +0100

    [FIX] after task grunt

--- dist/monkjs.js
+++ dist/monkjs.js
@@ -1,197 +1,207 @@
 (function() {
-  (function() {
-    (function() {
-      var Class, Decorator, Policies, fn, root;
-      Class = void 0;
-      Decorator = void 0;
-      Policies = void 0;
-      fn = void 0;
-      root = void 0;
-      fn = {
-        flip: function(fn) {
-          return function(first, second) {
-            if (arguments.length === 2) {
-              return fn.call(second, first);
-            } else {
-              return function(second) {
-                return fn.call(second, first);
-              };
-            }
-          };
-        },
-        extend: function(consumer, provider) {
-          var key;
-          key = void 0;
-          for (key in provider) {
-            if (provider.hasOwnProperty(key)) {
-              consumer[key] = provider[key];
-            }
-          }
-          return consumer;
-        },
-        include: function(consumer, provider) {
-          var key;
-          key = void 0;
-          for (key in provider) {
-            if (provider.hasOwnProperty(key)) {
-              consumer.prototype[key] = provider[key];
-            }
-          }
-          return consumer;
-        }
-      };
-      Class = function() {
-        var Child, F, Parent, attributes, i, props;
-        Child = void 0;
-        F = void 0;
-        Parent = void 0;
-        attributes = void 0;
-        i = void 0;
-        props = void 0;
-        if (arguments.length === 0) {
-          throw new Error("arguments null");
-        }
-        if (arguments.length === 1) {
-          Parent = null;
-          props = arguments[0];
+  var Class, Decorator, Policies, fn, root;
+
+  Class = void 0;
+
+  Decorator = void 0;
+
+  Policies = void 0;
+
+  fn = void 0;
+
+  root = void 0;
+
+  fn = {
+    flip: function(fn) {
+      return function(first, second) {
+        if (arguments.length === 2) {
+          return fn.call(second, first);
         } else {
-          Parent = arguments[0];
-          props = arguments[1];
-        }
-        if (props.hasOwnProperty("attributes")) {
-          attributes = props.attributes;
-          delete props.attributes;
-        }
-        Child = function() {
-          if (attributes !== undefined) {
-            if (typeof attributes === "object") {
-              fn.extend(this, attributes);
-            }
-          }
-          if (Child.surrogate && Child.surrogate.hasOwnProperty("initialize")) {
-            Child.surrogate.initialize.apply(this, arguments);
-          }
-          if (Child.prototype.hasOwnProperty("initialize")) {
-            Child.prototype.initialize.apply(this, arguments);
-          }
-        };
-        Child.extendClass = function(obj) {
-          return fn.extend(Child, obj);
-        };
-        Child.include = function(obj) {
-          return fn.include(Child, obj);
-        };
-        Parent = Parent || Object;
-        F = function() {};
-        F.prototype = Parent.prototype;
-        Child.prototype = new F();
-        Child.surrogate = Parent.prototype;
-        Child.prototype.$constructor = Child;
-        Child.prototype.$extend = function(obj) {
-          return fn.extend(this, obj);
-        };
-        Child.prototype.$watch = function(prop, handler) {
-          var getter, newval, oldval, setter;
-          getter = void 0;
-          newval = void 0;
-          oldval = void 0;
-          setter = void 0;
-          oldval = this[prop];
-          newval = oldval;
-          getter = function() {
-            return newval;
-          };
-          setter = function(val) {
-            oldval = newval;
-            return newval = handler.call(this, prop, oldval, val);
-          };
-          if (delete this[prop]) {
-            Object.defineProperty(this, prop, {
-              get: getter,
-              set: setter,
-              enumerable: true,
-              configurable: true
-            });
-          }
-          return this;
-        };
-        Child.prototype.$unwatch = function(prop) {
-          var val;
-          val = void 0;
-          val = this[prop];
-          delete this[prop];
-          this[prop] = val;
-          return this;
-        };
-        Child.prototype.$getAttributes = function() {
-          var array, key, obj;
-          array = void 0;
-          key = void 0;
-          obj = void 0;
-          array = [];
-          for (key in this) {
-            if (typeof this[key] !== "function") {
-              obj = {};
-              obj[key] = this[key];
-              array.push(obj);
-            }
-          }
-          return array;
-        };
-        return fn.include(Child, props);
-      };
-      Decorator = function(decoration) {
-        return function(clazz) {
-          var Decorated, deco, instance, key;
-          Decorated = void 0;
-          deco = void 0;
-          instance = void 0;
-          key = void 0;
-          Decorated = function() {
-            var self;
-            self = void 0;
-            self = (this instanceof Decorated ? this : new Decorated());
-            return clazz.apply(self, arguments);
-          };
-          key = void 0;
-          instance = new clazz();
-          deco = new Decorated();
-          for (key in instance) {
-            if (deco.hasOwnProperty(key)) {
-              delete instance[key];
-            }
-          }
-          Decorated.prototype = fn.extend(instance, decoration);
-          return Decorated;
-        };
-      };
-      Policies = {
-        after: function(decoration) {
-          return function(method) {
-            return function() {
-              var value;
-              value = void 0;
-              value = method.apply(this, arguments);
-              decoration.call(this, value);
-              return value;
-            };
-          };
-        },
-        before: function(decoration) {
-          return function(method) {
-            return function() {
-              decoration.apply(this, arguments);
-              return method.apply(this, arguments);
-            };
+          return function(second) {
+            return fn.call(second, first);
           };
         }
       };
-      root = (typeof exports !== "undefined" && exports !== null ? exports : window);
-      root.$Class = Class;
-      root.$Decorator = Decorator;
-      root.$Policies = Policies;
-      root.$fn = fn;
-    }).call(this);
-  }).call(this);
+    },
+    extend: function(consumer, provider) {
+      var key;
+      key = void 0;
+      for (key in provider) {
+        if (provider.hasOwnProperty(key)) {
+          consumer[key] = provider[key];
+        }
+      }
+      return consumer;
+    },
+    include: function(consumer, provider) {
+      var key;
+      key = void 0;
+      for (key in provider) {
+        if (provider.hasOwnProperty(key)) {
+          consumer.prototype[key] = provider[key];
+        }
+      }
+      return consumer;
+    }
+  };
+
+  Class = function() {
+    var Child, F, Parent, attributes, i, props;
+    Child = void 0;
+    F = void 0;
+    Parent = void 0;
+    attributes = void 0;
+    i = void 0;
+    props = void 0;
+    if (arguments.length === 0) {
+      throw new Error("arguments null");
+    }
+    if (arguments.length === 1) {
+      Parent = null;
+      props = arguments[0];
+    } else {
+      Parent = arguments[0];
+      props = arguments[1];
+    }
+    if (props.hasOwnProperty("attributes")) {
+      attributes = props.attributes;
+      delete props.attributes;
+    }
+    Child = function() {
+      if (attributes !== undefined) {
+        if (typeof attributes === "object") {
+          fn.extend(this, attributes);
+        }
+      }
+      if (Child.surrogate && Child.surrogate.hasOwnProperty("initialize")) {
+        Child.surrogate.initialize.apply(this, arguments);
+      }
+      if (Child.prototype.hasOwnProperty("initialize")) {
+        Child.prototype.initialize.apply(this, arguments);
+      }
+    };
+    Child.extendClass = function(obj) {
+      return fn.extend(Child, obj);
+    };
+    Child.include = function(obj) {
+      return fn.include(Child, obj);
+    };
+    Parent = Parent || Object;
+    F = function() {};
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.surrogate = Parent.prototype;
+    Child.prototype.$constructor = Child;
+    Child.prototype.$extend = function(obj) {
+      return fn.extend(this, obj);
+    };
+    Child.prototype.$watch = function(prop, handler) {
+      var getter, newval, oldval, setter;
+      getter = void 0;
+      newval = void 0;
+      oldval = void 0;
+      setter = void 0;
+      oldval = this[prop];
+      newval = oldval;
+      getter = function() {
+        return newval;
+      };
+      setter = function(val) {
+        oldval = newval;
+        return newval = handler.call(this, prop, oldval, val);
+      };
+      if (delete this[prop]) {
+        Object.defineProperty(this, prop, {
+          get: getter,
+          set: setter,
+          enumerable: true,
+          configurable: true
+        });
+      }
+      return this;
+    };
+    Child.prototype.$unwatch = function(prop) {
+      var val;
+      val = void 0;
+      val = this[prop];
+      delete this[prop];
+      this[prop] = val;
+      return this;
+    };
+    Child.prototype.$getAttributes = function() {
+      var array, key, obj;
+      array = void 0;
+      key = void 0;
+      obj = void 0;
+      array = [];
+      for (key in this) {
+        if (typeof this[key] !== "function") {
+          obj = {};
+          obj[key] = this[key];
+          array.push(obj);
+        }
+      }
+      return array;
+    };
+    return fn.include(Child, props);
+  };
+
+  Decorator = function(decoration) {
+    return function(clazz) {
+      var Decorated, deco, instance, key;
+      Decorated = void 0;
+      deco = void 0;
+      instance = void 0;
+      key = void 0;
+      Decorated = function() {
+        var self;
+        self = void 0;
+        self = (this instanceof Decorated ? this : new Decorated());
+        return clazz.apply(self, arguments);
+      };
+      key = void 0;
+      instance = new clazz();
+      deco = new Decorated();
+      for (key in instance) {
+        if (deco.hasOwnProperty(key)) {
+          delete instance[key];
+        }
+      }
+      Decorated.prototype = fn.extend(instance, decoration);
+      return Decorated;
+    };
+  };
+
+  Policies = {
+    after: function(decoration) {
+      return function(method) {
+        return function() {
+          var value;
+          value = void 0;
+          value = method.apply(this, arguments);
+          decoration.call(this, value);
+          return value;
+        };
+      };
+    },
+    before: function(decoration) {
+      return function(method) {
+        return function() {
+          decoration.apply(this, arguments);
+          return method.apply(this, arguments);
+        };
+      };
+    }
+  };
+
+  root = (typeof exports !== "undefined" && exports !== null ? exports : window);
+
+  root.$Class = Class;
+
+  root.$Decorator = Decorator;
+
+  root.$Policies = Policies;
+
+  root.$fn = fn;
 
 }).call(this);
--- src/monkjs.coffee
+++ src/monkjs.coffee
@@ -1,155 +1,149 @@
-(->
-  (->
-    Class = undefined
-    Decorator = undefined
-    Policies = undefined
-    fn = undefined
-    root = undefined
-    fn =
-      flip: (fn) ->
-        (first, second) ->
-          if arguments.length is 2
-            fn.call second, first
-          else
-            (second) ->
-              fn.call second, first
-
-      extend: (consumer, provider) ->
-        key = undefined
-        for key of provider
-          consumer[key] = provider[key]  if provider.hasOwnProperty(key)
-        consumer
-
-      include: (consumer, provider) ->
-        key = undefined
-        for key of provider
-          consumer::[key] = provider[key]  if provider.hasOwnProperty(key)
-        consumer
-
-    Class = ->
-      Child = undefined
-      F = undefined
-      Parent = undefined
-      attributes = undefined
-      i = undefined
-      props = undefined
-      throw new Error("arguments null")  if arguments.length is 0
-      if arguments.length is 1
-        Parent = null
-        props = arguments[0]
+Class = undefined
+Decorator = undefined
+Policies = undefined
+fn = undefined
+root = undefined
+fn =
+  flip: (fn) ->
+    (first, second) ->
+      if arguments.length is 2
+        fn.call second, first
       else
-        Parent = arguments[0]
-        props = arguments[1]
-      if props.hasOwnProperty("attributes")
-        attributes = props.attributes
-        delete props.attributes
-      Child = ->
-        fn.extend this, attributes  if typeof attributes is "object"  if attributes isnt `undefined`
-        Child.surrogate.initialize.apply this, arguments  if Child.surrogate and Child.surrogate.hasOwnProperty("initialize")
-        Child::initialize.apply this, arguments  if Child::hasOwnProperty("initialize")
-        return
+        (second) ->
+          fn.call second, first
 
-      Child.extendClass = (obj) ->
-        fn.extend Child, obj
+  extend: (consumer, provider) ->
+    key = undefined
+    for key of provider
+      consumer[key] = provider[key]  if provider.hasOwnProperty(key)
+    consumer
 
-      Child.include = (obj) ->
-        fn.include Child, obj
+  include: (consumer, provider) ->
+    key = undefined
+    for key of provider
+      consumer::[key] = provider[key]  if provider.hasOwnProperty(key)
+    consumer
 
-      Parent = Parent or Object
-      F = ->
-
-      F:: = Parent::
-      Child:: = new F()
-      Child.surrogate = Parent::
-      Child::$constructor = Child
-      Child::$extend = (obj) ->
-        fn.extend this, obj
-
-      Child::$watch = (prop, handler) ->
-        getter = undefined
-        newval = undefined
-        oldval = undefined
-        setter = undefined
-        oldval = this[prop]
-        newval = oldval
-        getter = ->
-          newval
-
-        setter = (val) ->
-          oldval = newval
-          newval = handler.call(this, prop, oldval, val)
-
-        if delete this[prop]
-          Object.defineProperty this, prop,
-            get: getter
-            set: setter
-            enumerable: true
-            configurable: true
-
-        this
-
-      Child::$unwatch = (prop) ->
-        val = undefined
-        val = this[prop]
-        delete this[prop]
-
-        this[prop] = val
-        this
-
-      Child::$getAttributes = ->
-        array = undefined
-        key = undefined
-        obj = undefined
-        array = []
-        for key of this
-          if typeof this[key] isnt "function"
-            obj = {}
-            obj[key] = this[key]
-            array.push obj
-        array
-
-      fn.include Child, props
-
-    Decorator = (decoration) ->
-      (clazz) ->
-        Decorated = undefined
-        deco = undefined
-        instance = undefined
-        key = undefined
-        Decorated = ->
-          self = undefined
-          self = ((if this instanceof Decorated then this else new Decorated()))
-          clazz.apply self, arguments
-
-        key = undefined
-        instance = new clazz()
-        deco = new Decorated()
-        for key of instance
-          delete instance[key]  if deco.hasOwnProperty(key)
-        Decorated:: = fn.extend(instance, decoration)
-        Decorated
-
-    Policies =
-      after: (decoration) ->
-        (method) ->
-          ->
-            value = undefined
-            value = method.apply(this, arguments)
-            decoration.call this, value
-            value
-
-      before: (decoration) ->
-        (method) ->
-          ->
-            decoration.apply this, arguments
-            method.apply this, arguments
-
-    root = ((if typeof exports isnt "undefined" and exports isnt null then exports else window))
-    root.$Class = Class
-    root.$Decorator = Decorator
-    root.$Policies = Policies
-    root.$fn = fn
+Class = ->
+  Child = undefined
+  F = undefined
+  Parent = undefined
+  attributes = undefined
+  i = undefined
+  props = undefined
+  throw new Error("arguments null")  if arguments.length is 0
+  if arguments.length is 1
+    Parent = null
+    props = arguments[0]
+  else
+    Parent = arguments[0]
+    props = arguments[1]
+  if props.hasOwnProperty("attributes")
+    attributes = props.attributes
+    delete props.attributes
+  Child = ->
+    fn.extend this, attributes  if typeof attributes is "object"  if attributes isnt `undefined`
+    Child.surrogate.initialize.apply this, arguments  if Child.surrogate and Child.surrogate.hasOwnProperty("initialize")
+    Child::initialize.apply this, arguments  if Child::hasOwnProperty("initialize")
     return
-  ).call this
-  return
-).call this
+
+  Child.extendClass = (obj) ->
+    fn.extend Child, obj
+
+  Child.include = (obj) ->
+    fn.include Child, obj
+
+  Parent = Parent or Object
+  F = ->
+
+  F:: = Parent::
+  Child:: = new F()
+  Child.surrogate = Parent::
+  Child::$constructor = Child
+  Child::$extend = (obj) ->
+    fn.extend this, obj
+
+  Child::$watch = (prop, handler) ->
+    getter = undefined
+    newval = undefined
+    oldval = undefined
+    setter = undefined
+    oldval = this[prop]
+    newval = oldval
+    getter = ->
+      newval
+
+    setter = (val) ->
+      oldval = newval
+      newval = handler.call(this, prop, oldval, val)
+
+    if delete this[prop]
+      Object.defineProperty this, prop,
+        get: getter
+        set: setter
+        enumerable: true
+        configurable: true
+
+    this
+
+  Child::$unwatch = (prop) ->
+    val = undefined
+    val = this[prop]
+    delete this[prop]
+
+    this[prop] = val
+    this
+
+  Child::$getAttributes = ->
+    array = undefined
+    key = undefined
+    obj = undefined
+    array = []
+    for key of this
+      if typeof this[key] isnt "function"
+        obj = {}
+        obj[key] = this[key]
+        array.push obj
+    array
+
+  fn.include Child, props
+
+Decorator = (decoration) ->
+  (clazz) ->
+    Decorated = undefined
+    deco = undefined
+    instance = undefined
+    key = undefined
+    Decorated = ->
+      self = undefined
+      self = ((if this instanceof Decorated then this else new Decorated()))
+      clazz.apply self, arguments
+
+    key = undefined
+    instance = new clazz()
+    deco = new Decorated()
+    for key of instance
+      delete instance[key]  if deco.hasOwnProperty(key)
+    Decorated:: = fn.extend(instance, decoration)
+    Decorated
+
+Policies =
+  after: (decoration) ->
+    (method) ->
+      ->
+        value = undefined
+        value = method.apply(this, arguments)
+        decoration.call this, value
+        value
+
+  before: (decoration) ->
+    (method) ->
+      ->
+        decoration.apply this, arguments
+        method.apply this, arguments
+
+root = ((if typeof exports isnt "undefined" and exports isnt null then exports else window))
+root.$Class = Class
+root.$Decorator = Decorator
+root.$Policies = Policies
+root.$fn = fn
